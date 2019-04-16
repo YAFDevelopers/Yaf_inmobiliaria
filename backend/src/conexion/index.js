f5f555a0fcac0b5c99867c28869858503664eb83
Yeisson Jimenez <yeisson310@hotmail.com>
2019-04-16 14:10:38 -0500

    Arquitectura backend montada

---
+++ backend/src/conexion/index.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+var settings = require('./config.json');
+var connection;
+
+function connectDatabase() 
+{
+  if(!connection) 
+  {
+    connection = mysql.createConnection(settings);
+
+    connection.connect(function(err)
+    {
+      if(!err) 
+      {
+        console.log('Base de Datos Conectada');
+      } 
+      else 
+      {
+        console.log('Error en la conexión con la Base de Datos');
+      }
+    });
+  }
+  return connection;
+}
+
+module.exports = connectDatabase();
