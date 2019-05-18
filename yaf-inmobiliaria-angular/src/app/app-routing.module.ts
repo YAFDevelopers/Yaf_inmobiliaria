f5c13432715be80db188a9193878bf715e4cc6ee
Sebastian Fonseca <sebastianfonseca@hotmail.com>
2019-05-18 11:01:55 -0500

    se implemento la plantilla al angular

--- yaf-inmobiliaria-angular/src/app/app-routing.module.ts
+++ yaf-inmobiliaria-angular/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InicioComponent} from './inicio/inicio.component';
+import { CitasComponent } from './citas/citas.component'; 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', 
+    component:InicioComponent
+  },
+    {
+      path:'citas',
+      component:CitasComponent
+
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
