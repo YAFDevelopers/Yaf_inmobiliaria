7575fb599c3b68b0c2e7a635138bf3ca8ad7c0fc
Sebastian Fonseca <sebastianfonseca@hotmail.com>
2019-05-20 10:15:13 -0500

    se agrego citas,agenetes y ayuda al angular

--- yaf-inmobiliaria-angular/src/app/app-routing.module.ts
+++ yaf-inmobiliaria-angular/src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {InicioComponent} from './inicio/inicio.component';
 import { CitasComponent } from './citas/citas.component'; 
-
+import {AyudaComponent} from './ayuda/ayuda.component';
+import{AgenteComponent} from './agente/agente.component';
 const routes: Routes = [
   {
     path:'', 
@@ -11,7 +12,14 @@ const routes: Routes = [
     {
       path:'citas',
       component:CitasComponent
-
+    },
+    {
+      path:'ayuda',
+      component:AyudaComponent
+    },
+    {
+      path:'agente',
+      component:AgenteComponent
     }
 ];
 
